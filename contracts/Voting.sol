cc7e6f461af42c9b49b33c69b8de92abb3cca59c
David Dada <daveproxy80@gmail.com>
2024-02-24 02:50:11 +0100

    sketching

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.21;
 
 contract Voting {
+    struct Poll {
+        mapping(address => uint) candidateVotes;
+        address[] candidates;
+        uint duration;
+    }
 
+    function createPoll(address[] _candidates, uint _duration) external {
+        Poll poll = Poll(_candidates, _duration);
+    }
+
+    function castVote(address _candidate) external {
+        candidateVotes[_candidate] = candidateVotes[_candidate] + 1;
+
+    }
 }
