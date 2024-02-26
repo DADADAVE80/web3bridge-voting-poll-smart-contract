b1afdf23e16638cdabab396a0d3f59441070bafe
David Dada <daveproxy80@gmail.com>
2024-02-26 12:24:58 +0100

    update logic and add cast vote logic

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,18 +2,27 @@
 pragma solidity ^0.8.21;
 
 contract Voting {
-    struct Poll {
-        mapping(address => uint) candidateVotes;
-        address[] candidates;
-        uint duration;
+    mapping(address => mapping(address => uint256)) candidateVotes;
+    address[] candidates;
+    uint256 duration;
+
+    function addCandidate(address _candidate) external {
+        candidates.push(_candidate);
     }
 
-    function createPoll(address[] _candidates, uint _duration) external {
-        Poll poll = Poll(_candidates, _duration);
+    function showCandidates() external view returns (address[] memory) {
+        return candidates;
     }
 
     function castVote(address _candidate) external {
-        candidateVotes[_candidate] = candidateVotes[_candidate] + 1;
-
+        for (uint i = 0; i < candidates.length; i++) {
+            if (candidates[i] == _candidate) {
+                if (candidateVotes[msg.sender][_candidate] == 0) {
+                    candidateVotes[msg.sender][_candidate] += 1;
+                } else {
+                    revert("You have already voted for this candidate");
+                }
+            }
+        }
     }
 }
